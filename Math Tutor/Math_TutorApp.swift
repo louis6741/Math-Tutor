3508ff556a6503295f9c4f9b7f606196af0ffe64
louis6741 <lonigrolouis999@gmail.com>
2026-02-09 12:37:38 -0800

    Initial Commit

---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by LONIGRO, LOUIS on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
